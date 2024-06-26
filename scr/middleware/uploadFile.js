35ae383ae20eace56af7937406f2070ac87a9ef3
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-06-26 18:09:00 +0200

    dynamic folder based on author or books

--- scr/middleware/uploadFile.js
+++ scr/middleware/uploadFile.js
@@ -2,10 +2,22 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+/* const getFolderName = (req) => {
+    if (req.path.includes('authors')) {
+        return 'Books-Authors-api-rest-file/authors';
+    } else if (req.path.includes('books')) {
+        return 'Books-Authors-api-rest-file/books';
+    }
+    return 'Books-Authors-api-rest-file/others'; // Default folder
+}; */
+
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: "Books-Authors-api-rest-file",
+  /*       folder: (req) => {
+            getFolderName(req);
+        },      */
         allowedFormats: ["jpg", "png", "jpeg", "gif"]
     }
 });
